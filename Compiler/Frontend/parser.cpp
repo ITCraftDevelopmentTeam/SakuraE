e5737200b25bf088b7c9607e7a42b317aa9e852c
powerangelXD <692732163@qq.com>
2025-11-17 17:25:39 +0800

    feat: 'genResource' methods implemented here

---
+++ Compiler/Frontend/parser.cpp
@@ -0,0 +1,228 @@
+#include "parser.hpp"
+
+sakoraE::NodePtr sakoraE::LiteralParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::LiteralNode);
+
+    (*root)[ASTTag::Literal] = std::visit([](auto& ptr) -> NodePtr {
+        std::shared_ptr<Token> tok = ptr->token;
+        return std::make_shared<Node>(tok);
+    }, option());
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::IndexOpParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::IndexOpNode);
+
+    (*root)[ASTTag::HeadExpr] = std::get<1>(getTuple())->genResource();
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::CallingOpParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::CallingOpNode);
+
+    if (std::get<1>(getTuple())->isEmpty()) return root;
+    else {
+        (*root)[ASTTag::Exprs]->addChild(std::get<1>(getTuple())->
+                                        getClosure().at(0)->
+                                        genResource());
+
+        auto closure = std::get<2>(getTuple());
+        if (closure->isEmpty()) return root;
+        else {
+            for (auto unit: closure->getClosure()) {
+                (*root)[ASTTag::Exprs]->addChild(std::get<1>(unit->getTuple())->genResource());
+            }
+        }
+    }
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::AtomIdentifierExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::CallingOpNode);
+
+    (*root)[ASTTag::Identifier] = std::make_shared<Node>(std::get<0>(getTuple())->token);
+    
+    auto closure = std::get<1>(getTuple());
+    if (closure->isEmpty()) return root; // No operator existing, return
+    else {
+        // Generating operators
+        for (auto unit: closure->getClosure()) {
+            auto op = std::visit([](auto& ptr) -> NodePtr {
+                return ptr->genResource();
+            }, unit->option());
+            (*root)[ASTTag::Ops]->addChild(op);
+        }
+    }
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::IdentifierExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::IdentifierExprNode);
+    auto not_op = std::get<0>(getTuple());
+    if (!not_op->isEmpty())
+        (*root)[ASTTag::Op] = std::make_shared<Node>(not_op->getClosure().at(0)->token);
+
+    (*root)[ASTTag::Exprs]->addChild(std::get<1>(getTuple())->genResource());
+    
+    auto subs = std::get<2>(getTuple());
+    if (!subs->isEmpty()) {
+        for (auto unit: subs->getClosure()) {
+            (*root)[ASTTag::Exprs]->addChild(std::get<1>(unit->getTuple())->genResource());
+        }
+    }
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::PrimExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::PrimExprNode);
+    
+    std::visit([&](auto& var) {
+        using VarType = std::decay_t<decltype(var)>;
+
+        if constexpr (std::is_same_v<VarType, std::shared_ptr<LiteralParser>>) {
+            (*root)[ASTTag::Literal] = var->genResource();
+        }
+        else if constexpr (std::is_same_v<VarType, std::shared_ptr<IdentifierExprParser>>) {
+            (*root)[ASTTag::Identifier] = var->genResource();
+        }
+        else {
+            (*root)[ASTTag::HeadExpr] = std::get<1>(var->getTuple())->genResource();
+        }
+
+    }, option());
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::MulExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::MulExprNode);
+
+    (*root)[ASTTag::Exprs]->addChild(std::get<0>(getTuple())->genResource());
+
+    auto subs = std::get<1>(getTuple());
+    if (!subs->isEmpty()) {
+        for (auto unit: subs->getClosure()) {
+            (*root)[ASTTag::Exprs]->addChild(std::get<1>(unit->getTuple())->genResource());
+            (*root)[ASTTag::Ops]->addChild(std::visit([=](auto& var) -> NodePtr {
+                std::shared_ptr<Token> tok = var->token;
+                return std::make_shared<Node>(tok);
+            },std::get<0>(unit->getTuple())->option()));
+        }
+    }
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::AddExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::AddExprNode);
+
+    (*root)[ASTTag::Exprs]->addChild(std::get<0>(getTuple())->genResource());
+
+    auto subs = std::get<1>(getTuple());
+    if (!subs->isEmpty()) {
+        for (auto unit: subs->getClosure()) {
+            (*root)[ASTTag::Exprs]->addChild(std::get<1>(unit->getTuple())->genResource());
+            (*root)[ASTTag::Ops]->addChild(std::visit([=](auto& var) -> NodePtr {
+                std::shared_ptr<Token> tok = var->token;
+                return std::make_shared<Node>(tok);
+            },std::get<0>(unit->getTuple())->option()));
+        }
+    }
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::LogicExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::LogicExprNode);
+
+    (*root)[ASTTag::Exprs]->addChild(std::get<0>(getTuple())->genResource());
+
+    auto subs = std::get<1>(getTuple());
+    if (!subs->isEmpty()) {
+        for (auto unit: subs->getClosure()) {
+            (*root)[ASTTag::Exprs]->addChild(std::get<1>(unit->getTuple())->genResource());
+            (*root)[ASTTag::Ops]->addChild(std::visit([=](auto& var) -> NodePtr {
+                std::shared_ptr<Token> tok = var->token;
+                return std::make_shared<Node>(tok);
+            },std::get<0>(unit->getTuple())->option()));
+        }
+    }
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::BoolExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::BoolExprNode);
+
+    (*root)[ASTTag::Exprs]->addChild(std::get<0>(getTuple())->genResource());
+
+    auto subs = std::get<1>(getTuple());
+    if (!subs->isEmpty()) {
+        for (auto unit: subs->getClosure()) {
+            (*root)[ASTTag::Exprs]->addChild(std::get<1>(unit->getTuple())->genResource());
+            (*root)[ASTTag::Ops]->addChild(std::visit([=](auto& var) -> NodePtr {
+                std::shared_ptr<Token> tok = var->token;
+                return std::make_shared<Node>(tok);
+            },std::get<0>(unit->getTuple())->option()));
+        }
+    }
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::ArrayExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::ArrayExprNode);
+
+    auto checker_expr = std::get<1>(getTuple());
+    if (!checker_expr->isEmpty()) {
+        (*root)[ASTTag::Exprs]->addChild(checker_expr->getClosure().at(0)->genResource());
+    }
+
+    auto subs = std::get<2>(getTuple());
+    if (!subs->isEmpty()) {
+        for (auto unit: subs->getClosure()) {
+            (*root)[ASTTag::Exprs]->addChild(std::get<1>(unit->getTuple())->genResource());
+        }
+    }
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::AssignExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::AssignExprNode);
+
+    (*root)[ASTTag::Identifier] = std::get<0>(getTuple())->genResource();
+    (*root)[ASTTag::Op] = std::make_shared<Node>(std::get<1>(getTuple())->token);
+    (*root)[ASTTag::HeadExpr]->addChild(std::get<2>(getTuple())->genResource());
+
+    return root;
+}
+
+sakoraE::NodePtr sakoraE::WholeExprParser::genResource() {
+    NodePtr root = std::make_shared<Node>(ASTTag::WholeExprNode);
+
+    std::visit([&](auto& var) {
+        using VarType = std::decay_t<decltype(var)>;
+
+        if constexpr (std::is_same_v<VarType, std::shared_ptr<AddExprParser>>) {
+            (*root)[ASTTag::AddExprNode] = var->genResource();
+        }
+        else if constexpr (std::is_same_v<VarType, std::shared_ptr<BoolExprParser>>) {
+            (*root)[ASTTag::BoolExprNode] = var->genResource();
+        }
+        else if constexpr (std::is_same_v<VarType, std::shared_ptr<ArrayExprParser>>) {
+            (*root)[ASTTag::ArrayExprNode] = var->genResource();
+        }
+        else if constexpr (std::is_same_v<VarType, std::shared_ptr<AssignExprParser>>) {
+            (*root)[ASTTag::AssignExprNode] = var->genResource();
+        }
+
+    }, option());
+
+    return root;
+}
