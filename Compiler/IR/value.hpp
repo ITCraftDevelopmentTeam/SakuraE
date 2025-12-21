4287833f155835d661f183a367d07c7ae4db1035
powerangelXD <692732163@qq.com>
2025-12-21 23:49:50 +0800

    feat: Basically finish the ValueVariant Designing

--- Compiler/IR/value.hpp
+++ Compiler/IR/value.hpp
@@ -1,10 +1,21 @@
 #ifndef SAKORA_VALUE_HPP
 #define SAKORA_VALUE_HPP
 
-#include <iostream>
+#include "type/type.hpp"
 
 namespace sakoraE::IR {
-    
+    class ValueVariant {
+        std::variant<std::monostate, 
+                    int, 
+                    char,
+                    double, 
+                    std::string> value;
+    public:
+        ValueVariant(int val): value(val) {}
+        ValueVariant(char val): value(val) {}
+        ValueVariant(double val): value(val) {}
+        ValueVariant(std::string val): value(val) {}
+    };
 }
 
 #endif // !SAKORA_VALUE_HPP
