fef5f8bed740c3d8b017d8e092e244c736a1c1ef
powerangelXD <692732163@qq.com>
2025-12-23 13:21:03 +0800

    refactor: Rename value.hpp to symbol.hpp

---
+++ Compiler/IR/symbol.hpp
@@ -0,0 +1,28 @@
+#ifndef SAKORA_SYMBOL_HPP
+#define SAKORA_SYMBOL_HPP
+
+#include <stack>
+#include <map>
+
+#include <llvm/IR/Value.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Verifier.h>
+
+#include "type/type.hpp"
+#include "includes/String.hpp"
+
+namespace sakoraE::IR {
+    struct Symbol {
+        llvm::Value* value = nullptr;
+        Type type;
+    };
+
+    using SymbolPair = std::pair<fzlib::String, Symbol>;
+
+    class SymbolManager {
+    };
+}
+
+#endif // !SAKORA_SYMBOL_HPP
