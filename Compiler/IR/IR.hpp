e50ca9b197ab5b1c03ff26da535804e899d16596
powerangelXD <692732163@qq.com>
2026-01-12 18:57:15 +0800

    feat: Add support to constant

--- Compiler/IR/IR.hpp
+++ Compiler/IR/IR.hpp
@@ -6,11 +6,20 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Verifier.h>
 
+#include "Compiler/Frontend/AST.hpp"
+#include "symbol.hpp"
+#include "value.hpp"
+
 namespace sakoraE::IR {
     using LLVMCtxPtr = std::unique_ptr<llvm::LLVMContext>;
     using LLVMModule = std::unique_ptr<llvm::Module>;
     using LLVMIRBuilder = std::unique_ptr<llvm::IRBuilder<>>;
-    struct IRManager {
+    class IRManager {
+        // Storage constants
+        std::vector<Value> constantPool;
+        // Point to the current constant
+        std::size_t cur = 0;
+    public:
         llvm::LLVMContext* context;
         llvm::Module* mainModule;
         llvm::IRBuilder<>* builder;
@@ -19,6 +28,15 @@ namespace sakoraE::IR {
             context = new llvm::LLVMContext();
             mainModule = new llvm::Module("main_module", *context);
             builder = new llvm::IRBuilder<>(*context);
+        }
+
+        void makeConstant(Token tok) {
+            constantPool.push_back(Value::make(tok));
+            cur ++;
+        }
+
+        const Value& getConstant() {
+            return constantPool.at(cur);
         }
     };
 }
