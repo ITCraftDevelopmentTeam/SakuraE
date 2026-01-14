a9d9d7da6aa0f027b1c8136e93b50b6f94aa4d75
powerangelXD <692732163@qq.com>
2026-01-15 00:22:00 +0800

    feat: Add name for Value

--- Compiler/IR/value/value.hpp
+++ Compiler/IR/value/value.hpp
@@ -9,11 +9,20 @@ namespace sakuraE::IR {
     class Value {
     protected:
         Type* type;
+        fzlib::String name;
     public:
         explicit Value(Type* type) : type(type) {}
         virtual ~Value() = default;
 
         Type* getType() const { return type; }
+
+        void setName(const fzlib::String& n) {
+            name = n;
+        }
+
+        const fzlib::String& getName() {
+            return name;
+        }
     };
 
 }
