d460b25467d63ef57fdeb0259a10e02633ee8bb1
powerangelXD <692732163@qq.com>
2026-01-15 02:29:37 +0800

    refactor: Separate scope system from 'function.hpp'

---
+++ Compiler/IR/struct/scope.hpp
@@ -0,0 +1,62 @@
+#ifndef SAKURAE_SCOPE_HPP
+#define SAKURAE_SCOPE_HPP
+
+#include <stack>
+#include <utility>
+
+#include "Compiler/IR/value/constant.hpp"
+
+namespace sakuraE::IR {
+    struct Symbol: public Value {
+        fzlib::String name;
+        Value* address;
+
+        Symbol(fzlib::String n, Value* addr, Type* t): name(n), address(addr), Value(t) {}
+    };
+
+    class Scope {
+        std::vector<std::map<fzlib::String, Symbol>> symbolTables;
+        std::size_t cursor = 0;
+
+        PositionInfo createInfo;
+
+        Scope* parent = nullptr;
+
+        std::map<fzlib::String, Symbol>& top() {
+            return symbolTables[cursor];
+        }
+    public:
+        Scope(PositionInfo info): createInfo(info) {
+            symbolTables.emplace_back();
+        }
+
+        void declare(fzlib::String n, Value* addr, Type* t) {
+            top().emplace(n, Symbol(n, addr, t));
+        }
+
+        void enter() {
+            symbolTables.emplace_back();
+            cursor ++;
+        }
+
+        void leave() {
+            symbolTables.pop_back();
+            cursor --;
+        }
+
+        Symbol* lookup(const fzlib::String& name) {
+            for (auto it = symbolTables.rbegin(); it != symbolTables.rend(); it --) {
+                if (it->find(name) != it->end()) {
+                    return &(*it)[name];
+                }
+            }
+
+            if (parent)
+                return parent->lookup(name);
+            
+            return nullptr;
+        }
+    };
+}
+
+#endif // !SAKURAE_SCOPE_HPP
