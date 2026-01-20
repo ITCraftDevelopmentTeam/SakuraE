94f6afd888efa013af76f99600f75f98efba87d7
powerangelXD <692732163@qq.com>
2026-01-20 14:21:06 +0800

    feat: Init

--- Compiler/LLVMCodegen/parts/InstCodegen.hpp
+++ Compiler/LLVMCodegen/parts/InstCodegen.hpp
@@ -1,4 +1,29 @@
 #ifndef SAKURAE_INSTCODEGEN_HPP
 #define SAKURAE_INSTCODEGEN_HPP
 
+#include <map>
+#include <memory>
+#include <vector>
+
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Transforms/Utils/PromoteMemToReg.h>
+#include <llvm/Transforms/Utils.h>
+
+#include "Compiler/IR/generator.hpp"
+
+namespace sakuraE::Codegen {
+    class InstructionCodegen {
+        
+    };
+}
+
 #endif // !SAKURAE_INSTCODEGEN_HPP
