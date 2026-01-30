6131a3d9f103cf2c0ccc6951a0739e4e28d0bbcd
powerangelXD <692732163@qq.com>
2026-01-30 14:20:05 +0800

    refactor: Consolidate into a single file

--- Compiler/LLVMCodegen/LLVMCodegenerator.hpp
+++ Compiler/LLVMCodegen/LLVMCodegenerator.hpp
@@ -20,36 +20,69 @@
 
 #include "Compiler/IR/generator.hpp"
 
-namespace sakuraE {
+namespace sakuraE::Codegen {
     class LLVMCodeGenerator {
         IR::Program* program;
-        // llvm basic
         llvm::LLVMContext* context = nullptr;
         llvm::IRBuilder<>* builder = nullptr;
-        std::vector<llvm::Module*> modules;
-        long cursor = -1;
+        llvm::Module* mod = nullptr;
+
+        std::map<IR::IRValue*, llvm::Value*> instructionMap;
+
+        // Get IRValue to llvm Value referrence
+        inline llvm::Value* getRef(IR::IRValue* sakIRVal) {
+            return instructionMap[sakIRVal];
+        }
+
+        // Create a new IRValue to llvm Value referrence
+        inline void store(IR::IRValue* sakIRVal, llvm::Value* llvmIRVal) {
+            instructionMap[sakIRVal] = llvmIRVal;
+        }
     public:
         LLVMCodeGenerator(IR::Program* p) {
             program = p;
             context = new llvm::LLVMContext();
             builder = new llvm::IRBuilder<>(*context);
-            modules.push_back(new llvm::Module(program->getID().c_str(), *context));
-            cursor ++;
         }
 
-        void startGenerate();
     private:
-        // Tools
-        long cur() {
-            return cursor;
+        llvm::Value* codegen(IR::Instruction* ins);
+
+        llvm::Value* toLLVMConstant(IR::Constant* constant) {
+            switch (constant->getType()->getIRTypeID()){
+                case IR::IRTypeID::IntegerTyID: {
+                    return llvm::ConstantInt::get(constant->getType()->toLLVMType(*context), constant->getContentValue<int>());
+                }
+                case IR::IRTypeID::FloatTyID:
+                    return llvm::ConstantFP::get(constant->getType()->toLLVMType(*context), constant->getContentValue<double>());
+                case IR::IRTypeID::PointerTyID: {
+                    auto ptrType = dynamic_cast<IR::IRPointerType*>(constant->getType());
+                    if (ptrType->getElementType() == IR::IRType::getCharTy()) {
+                        // Is String
+                        fzlib::String strVal = constant->getContentValue<fzlib::String>();
+                        return builder->CreateGlobalString(strVal.c_str());
+                    }
+                }
+                default:
+                    return nullptr;
+            }
         }
 
-        void moveCursor(long c) {
-            cursor = c;
+        void buildMapping(IR::Instruction* ins) {
+            llvm::Value* result = codegen(ins);
+            if (result) {
+                store(ins, result);
+            }
         }
 
-        llvm::Module* curModule() {
-            return modules[cursor];
+        llvm::Value* toLLVMValue(IR::IRValue* value) {
+            if (instructionMap.find(value) != instructionMap.end()) {
+                return getRef(value);
+            }
+            else if (auto* constant = dynamic_cast<IR::Constant*>(value)) {
+                return toLLVMConstant(constant);
+            }
+            return nullptr;
         }
     };
 }
