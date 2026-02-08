b1aa019febb3483dbb581a520060ab7b85e895ab
fzsgball <fzsgball@qq.com>
2026-02-08 15:34:06 +0800

    test

--- main.cpp
+++ main.cpp
@@ -16,8 +16,12 @@
 #include "Compiler/LLVMCodegen/LLVMCodegenerator.hpp"
 
 const fzlib::String SOURCE_CODE = R"(
+func foo(a: int, b: int) -> int {
+    return a * b;
+}
+
 func main(a: int, b: int) -> int {
-    let g = 10;
+    let g = foo(1, 2);
     for (let i=0; i<a+b; i++) {
         g += i*2;
     }
@@ -35,6 +39,8 @@ int main() {
             std::cout << t.toString() << "; size:" << t.toString().len() << std::endl;
         }
         sakuraE::TokenIter current = r.begin();
+        sakuraE::IR::IRGenerator generator("TestProject");
+
         while ((*current).type != sakuraE::TokenType::_EOF_) {
             auto result = sakuraE::StatementParser::parse(current, r.end());
             if (result.status == sakuraE::ParseStatus::FAILED) {
@@ -49,75 +55,56 @@ int main() {
 
             std::cout << res->toFormatString(true) << std::endl;
 
-            std::cout << "=================SakuraE IR Printer==================" << std::endl;
-
-            sakuraE::IR::IRGenerator generator("TestProject");
-
             generator.visitStmt(res);
-
-            std::cout << generator.toFormatString() << std::endl;
-
-            std::cout << "===================LLVM IR Printer===================" << std::endl;
-
             current = result.end;
-            auto program = generator.getProgram();
-
-            sakuraE::Codegen::LLVMCodeGenerator llvmCodegen(&program);
-
-            llvmCodegen.start();
-
-            std::cout << "===================Before Optimize===================" << std::endl;
-
-            llvmCodegen.print();
-
-            llvmCodegen.optimize();
-
-            std::cout << "===================After Optimize===================" << std::endl;
-
-            llvmCodegen.print();
-
-            auto module = llvmCodegen.getModules()[0];
-            llvm::Module* rawModule = module->content; 
-            auto modulePtr = std::unique_ptr<llvm::Module>(rawModule);
-
-            llvm::InitializeNativeTarget();
-            llvm::InitializeNativeTargetAsmPrinter();
-            llvm::InitializeNativeTargetAsmParser();
-
-            std::string errStr;
-            llvm::ExecutionEngine* EE = llvm::EngineBuilder(std::move(modulePtr))
-                                            .setErrorStr(&errStr)
-                                            .create();
-
-            if (!EE) {
-                std::cerr << "Failed to construct ExecutionEngine: " << errStr << std::endl;
-                return 1;
-            }
-
-            llvm::Function* mainFn = EE->FindFunctionNamed("main");
-            if (!mainFn) {
-                std::cerr << "Could not find main function in module" << std::endl;
-                return 1;
-            }
-
-            EE->finalizeObject();
-
-            auto addr = EE->getFunctionAddress("main");
-
-            SakuraFuncPtr func = reinterpret_cast<SakuraFuncPtr>(addr);
-
-            std::cout << "JIT Result: " << func(10,10) << std::endl;
-
-
-            int a=10,b=10;
-            int g=10;
-
-            for (int i=0;i<a+b;i++) {
-                g += i*2;
-            }
-
-            std::cout << "Real Result: " << g << std::endl;
         }
+
+        std::cout << "=================SakuraE IR Printer==================" << std::endl;
+
+
+        std::cout << generator.toFormatString() << std::endl;
+
+        std::cout << "===================LLVM IR Printer===================" << std::endl;
+
+        auto program = generator.getProgram();
+
+        sakuraE::Codegen::LLVMCodeGenerator llvmCodegen(&program);
+        llvmCodegen.start();
+        std::cout << "===================Before Optimize===================" << std::endl;
+        llvmCodegen.print();
+        llvmCodegen.optimize();
+        std::cout << "===================After Optimize===================" << std::endl;
+        llvmCodegen.print();
+        auto module = llvmCodegen.getModules()[0];
+        llvm::Module* rawModule = module->content; 
+        auto modulePtr = std::unique_ptr<llvm::Module>(rawModule);
+        llvm::InitializeNativeTarget();
+        llvm::InitializeNativeTargetAsmPrinter();
+        llvm::InitializeNativeTargetAsmParser();
+        std::string errStr;
+        llvm::ExecutionEngine* EE = llvm::EngineBuilder(std::move(modulePtr))
+                                        .setErrorStr(&errStr)
+                                        .create();
+
+        if (!EE) {
+            std::cerr << "Failed to construct ExecutionEngine: " << errStr << std::endl;
+            return 1;
+        }
+        llvm::Function* mainFn = EE->FindFunctionNamed("main");
+        if (!mainFn) {
+            std::cerr << "Could not find main function in module" << std::endl;
+            return 1;
+        }
+        EE->finalizeObject();
+        auto addr = EE->getFunctionAddress("main");
+        SakuraFuncPtr func = reinterpret_cast<SakuraFuncPtr>(addr);
+        std::cout << "JIT Result: " << func(10,10) << std::endl;
+        int a=10,b=10;
+        int g=2;
+        for (int i=0;i<a+b;i++) {
+            g += i*2;
+        }
+        std::cout << "Real Result: " << g << std::endl;
     } 
     catch (const std::runtime_error& e) {
         std::cerr << e.what() << "\n";
