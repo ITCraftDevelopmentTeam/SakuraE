5ee04695320495eb58e6b74ae8eacc815fb92610
fzsgball <fzsgball@qq.com>
2026-02-09 17:01:38 +0800

    feat: Add refs

--- Compiler/IR/struct/module.hpp
+++ Compiler/IR/struct/module.hpp
@@ -19,6 +19,8 @@ namespace sakuraE::IR {
         // Indicates the current maximum index of fnList
         long cursor = -1;
 
+        std::vector<Module*> refs;
+
         Program* program;
     public:
         Module(fzlib::String id, PositionInfo info):
