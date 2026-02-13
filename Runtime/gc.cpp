6b9f29b7da04cef1f39bdf959efbde7dcad34b08
fzsgball <fzsgball@qq.com>
2026-02-13 11:02:05 +0800

    feat: Add new gc runtime library

---
+++ Runtime/gc.cpp
@@ -0,0 +1,123 @@
+#include "gc.h"
+#include <algorithm>
+#include <atomic>
+#include <cstdint>
+#include <cstdlib>
+#include <mutex>
+#include <stack>
+
+namespace sakuraE::runtime {
+    extern "C" void   __gc_create_thread() {
+        std::lock_guard<std::mutex> lock(gc_mutex);
+        global_stacks.push_back(&own_stack);
+        total_active ++;
+    }
+
+    extern "C" void   __gc_safe_point() {
+        if (need_gc.load(std::memory_order_relaxed)) {
+            std::unique_lock<std::mutex> lock(gc_mutex);
+            safepoints ++;
+
+            gc_cv.notify_one();
+
+            resume_cv.wait(lock, [] { return !need_gc.load(); });
+
+            safepoints --;
+        }
+    }
+
+    extern "C" void*  __gc_alloc(size_t size, ObjectType ty) {
+        if (allocated_bytes + size > limit) {
+            bool expected = false;
+            if (need_gc.compare_exchange_strong(expected, true)) {
+                __gc_collect();
+            }
+            else {
+                __gc_safe_point();
+            }
+        }
+
+        ObjectHeader* header = (ObjectHeader*)malloc(sizeof(ObjectHeader) + size);
+        header->obj_size = size;
+        header->obj_status.store(Unscanned);
+        header->obj_type = ty;
+
+        std::lock_guard<std::mutex> lock(gc_mutex);
+        global_heap.push_back(header);
+        allocated_bytes.fetch_add(size);
+
+        return (void*)(header + 1);
+    }
+
+    extern "C" void   __gc_register(void** addr) {
+        own_stack.push_back(addr);
+    }
+
+    extern "C" void   __gc_pop(uint32_t times) {
+        for (uint32_t i = 0; i < times; i ++) {
+            if (!own_stack.empty()) own_stack.pop_back();
+        }
+    }
+
+    extern "C" void   __gc_scan(void* ptr) {
+        if (!ptr) return;
+
+        std::stack<void*> work_stack;
+        work_stack.push(ptr);
+
+        while (!work_stack.empty()) {
+            void* _ptr = work_stack.top();
+            work_stack.pop();
+
+            ObjectHeader* header = (ObjectHeader*)_ptr - 1;
+        
+            GCMark expected = Unscanned;
+            if (header->obj_status.compare_exchange_strong(expected, Uncomplete)) {
+                void** data = (void**)_ptr;
+                size_t element_size = header->obj_size / sizeof(void*);
+
+                for (size_t i = 0; i < element_size; i ++) {
+                    work_stack.push(data[i]);
+                }
+
+                header->obj_status.store(Marked);
+            }
+        }
+    }
+
+    extern "C" void   __gc_collect() {
+        std::unique_lock<std::mutex> lock(gc_mutex);
+
+        gc_cv.wait(lock, [] {
+            return safepoints == (total_active - 1);
+        });
+
+        for (auto* stk: global_stacks) {
+            for (void** addr: *stk) {
+                if (addr && *addr) __gc_scan(*addr);
+            }
+        }
+
+        auto it = global_heap.begin();
+        while (it != global_heap.end()) {
+            ObjectHeader* header = *it;
+            if (header->obj_status == Unscanned) {
+                free(header);
+                allocated_bytes.fetch_sub(header->obj_size);
+                it = global_heap.erase(it);
+            }
+            else {
+                header->obj_status = Unscanned;
+                it ++;
+            }
+        }
+
+        if (allocated_bytes > limit * 0.7) {
+            limit *= 2;
+        }
+
+        need_gc = false;
+        resume_cv.notify_all();
+    }
+
+}
---
+++ Runtime/gc.h
@@ -0,0 +1,62 @@
+/*
+    SakuraE Runtime Library
+    gc.h
+    2026-2-13
+
+    By FZSGBall
+*/
+
+#ifndef SAKURAE_RUNTIME_GC_H
+#define SAKURAE_RUNTIME_GC_H
+
+#include <condition_variable>
+#include <cstdint>
+#include <sys/types.h>
+#include <vector>
+#include <mutex>
+#include <stdint.h>
+#include <cstdlib>
+#include <atomic>
+
+namespace sakuraE::runtime {
+    enum ObjectType: uint32_t {
+        String, Array, Struct
+    };
+
+    enum GCMark: uint32_t {
+        Unscanned,
+        Uncomplete,
+        Marked
+    };
+
+    struct ObjectHeader {
+        ObjectType obj_type;
+        std::atomic<GCMark> obj_status;
+        uint64_t obj_size;
+    };
+
+    // status
+    static std::atomic<bool> need_gc {false};         
+    static std::atomic<int>  total_active {0};        
+    static std::atomic<int>  safepoints {0}; 
+    static std::condition_variable gc_cv;           
+    static std::condition_variable resume_cv;   
+
+    // alloc
+    static std::atomic<size_t> allocated_bytes {0};
+    static size_t limit = 1024 * 1024;
+    static thread_local std::vector<void**> own_stack; 
+    static std::vector<std::vector<void**>*> global_stacks;
+    static std::vector<ObjectHeader*> global_heap;
+    static std::mutex gc_mutex;
+
+    extern "C" void   __gc_create_thread();
+    extern "C" void   __gc_safe_point();
+    extern "C" void*  __gc_alloc(size_t size, ObjectType ty);
+    extern "C" void   __gc_register(void** addr);
+    extern "C" void   __gc_pop(uint32_t times);
+    extern "C" void   __gc_scan(void* ptr);
+    extern "C" void   __gc_collect();
+}
+
+#endif // !SAKURAE_RUNTIME_GC_H
