2fb315e94b50cea52666c1531e95e7edd8d75dc8
powerangelXD <692732163@qq.com>
2026-03-13 15:34:46 +0800

    feat: Register

--- atrI/commands.hpp
+++ atrI/commands.hpp
@@ -137,6 +137,9 @@ namespace atri::cmds {
         runtimeSymbols[JIT->mangleAndIntern("__gc_create_thread")] = { llvm::orc::ExecutorAddr::fromPtr(&sakuraE::runtime::__gc_create_thread), llvm::JITSymbolFlags::Exported };
         runtimeSymbols[JIT->mangleAndIntern("__gc_pop")] = { llvm::orc::ExecutorAddr::fromPtr(&sakuraE::runtime::__gc_pop), llvm::JITSymbolFlags::Exported };
         runtimeSymbols[JIT->mangleAndIntern("__gc_register")] = { llvm::orc::ExecutorAddr::fromPtr(&sakuraE::runtime::__gc_register), llvm::JITSymbolFlags::Exported };
+        runtimeSymbols[JIT->mangleAndIntern("__gc_get_atomic_type")] = { llvm::orc::ExecutorAddr::fromPtr(&sakuraE::runtime::__gc_get_atomic_type), llvm::JITSymbolFlags::Exported };
+        runtimeSymbols[JIT->mangleAndIntern("__gc_get_string_type")] = { llvm::orc::ExecutorAddr::fromPtr(&sakuraE::runtime::__gc_get_string_type), llvm::JITSymbolFlags::Exported };
+
 
         llvm::cantFail(JD.define(llvm::orc::absoluteSymbols(runtimeSymbols)));
 
